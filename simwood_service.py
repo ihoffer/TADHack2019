4be04b373a8beb6e68d31ab7ece26f473fd54166
Istvan Hoffer <hofferistvan@me.com>
2019-10-12 12:58:25 +0100

    Change origin number to reserved number

--- simwood_service.py
+++ simwood_service.py
@@ -7,7 +7,7 @@ url = "https://api.simwood.com/v3/"
 
 user = "2f4229c1a1bb38fcf87c58c461b08f173202b600"
 pw = "6c1f7c55c70acfecee16633175729fed2232c197"
-app_name = "eFrancesco"
+origin_number = 447537149203
 
 session = requests.Session()
 session.auth = (user, pw)
@@ -15,7 +15,7 @@ session.auth = (user, pw)
 def send_text(to, message):
     endpoint = url + "messaging/{}/sms".format(account_id)
     payload = {
-        "from": app_name,
+        "from": origin_number,
         "to": to,
         "message": message
     }
