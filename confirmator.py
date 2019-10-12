a9bdf3d22a76b66e67c87277e666e8645692e57a
mpjuhasz <juhaszmp@mensa.hu>
2019-10-12 15:35:28 +0100

    Confirmator 1000

--- confirmator.py
+++ confirmator.py
@@ -1,7 +1,20 @@
 import json
+from simwood_service import send_text
 
-mylist = [["Tonteria", "Matyi", 447887370010, 4],["Cuckoo", "Istvan", 447477468440, 8]]
-bookings = {"bookings":mylist}
+data = []
+with open('dbbookings.json', 'r') as f:
+    for line in f:
+        data.append(json.loads(line))
 
 with open('dbbookings.json', 'w') as f:
-  json.dump(bookings, f)
+    del f
+
+
+print("Sup Francesco, having a grand day? \nYou got some decisions to make fam.\n")
+for i in data:
+    s = i['name'] + " wants to go to " + i['clubName'] +" in a group of " + str(i['groupSize']) + "\n"
+    print(s)
+    print("Francescoed? [Y/N]")
+    dec = str(input())
+    if dec == 'Y':
+        send_text(i['phone'], "You're booked in fam. Prepare them moves!")
