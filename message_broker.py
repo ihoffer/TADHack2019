d11fa0dedc045e8ff0dd1c378db0bf8b631bd379
mpjuhasz <juhaszmp@mensa.hu>
2019-10-12 17:36:07 +0100

    message broker update

--- message_broker.py
+++ message_broker.py
@@ -2,15 +2,15 @@
 import random
 
 final_confirmations = [
-    "I got your deets down, will be in touch..."
+    "I got your deets down, will be in touch...", "Peak. Let me gather the intel for you...", "No worries bossman, give me a minute..."
 ]
 
 stage_confirmations = {
-    "club": ["Oh geez, good choice, one of my favorites."],
+    "club": ["Oh geez, good choice, one of my favorites.", "Top choice from a top man.", "Legend."],
 }
 
 stage_questions = {
-    "groupSize": ["What's your group size, m8?"]
+    "groupSize": ["What's your group size, m8?", "How many of ya?", "What's the size of your crowd?"]
 }
 
 def get_stage_confirmation(stage):
