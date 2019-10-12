eba2b726cc9dd956a6e8e47b53c433f0628a8c9a
Istvan Hoffer <hofferistvan@me.com>
2019-10-12 11:57:57 +0100

    Implement send_message via Simwood

---
+++ simwood_service.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+import requests
+
+account_id = 931220
+api_key = "2f4229c1a1bb38fcf87c58c461b08f173202b600"
+url = "https://api.simwood.com/v3/"
+
+user = "2f4229c1a1bb38fcf87c58c461b08f173202b600"
+pw = "6c1f7c55c70acfecee16633175729fed2232c197"
+app_name = "Test"
+
+session = requests.Session()
+session.auth = (user, pw)
+
+def send_text(to, message):
+    endpoint = url + "messaging/{}/sms".format(account_id)
+    print endpoint
+    payload = {
+        "from": app_name,
+        "to": to,
+        "message": message
+    }
+    print session.post(endpoint, json=payload)
