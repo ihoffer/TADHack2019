cd0fef3ad39e4f398c18b0648075ace469bc3e41
Istvan Hoffer <hofferistvan@me.com>
2019-10-12 12:55:07 +0100

    Basic system structure

--- simwood_service.py
+++ simwood_service.py
@@ -7,7 +7,7 @@ url = "https://api.simwood.com/v3/"
 
 user = "2f4229c1a1bb38fcf87c58c461b08f173202b600"
 pw = "6c1f7c55c70acfecee16633175729fed2232c197"
-app_name = "Test"
+app_name = "eFrancesco"
 
 session = requests.Session()
 session.auth = (user, pw)
