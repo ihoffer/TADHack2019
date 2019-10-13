4df37dd9b52deac0f48b5dcb4e1ec9db6dc41d4d
Istvan Hoffer <hofferistvan@me.com>
2019-10-13 10:29:17 +0100

    Add parties dor Sunday

--- db.py
+++ db.py
@@ -6,9 +6,9 @@ dbcont = {
 }
 
 dbclub = [
-    {"name": "Tonteria", "parties": {0:None, 1:None, 2:None, 3:None, 4:"Student Night", 5:"Baszataska", 6:None}},
-    {"name": "Cuckoo", "parties": {0:None, 1:None, 2:None, 3:None, 4:"Minimal Techno", 5:"Acid party", 6:None}},
-    {"name": "Toyroom", "parties": {0:None, 1:None, 2:None, 3:None, 4:None, 5:"Salsa night", 6:None}},
+    {"name": "Tonteria", "parties": {0:None, 1:None, 2:None, 3:None, 4:"Student Night", 5:None, 6:"Baszataska"}},
+    {"name": "Cuckoo", "parties": {0:None, 1:None, 2:None, 3:None, 4:"Minimal Techno", 5:None, 6:"Acid party"}},
+    {"name": "Toyroom", "parties": {0:None, 1:None, 2:None, 3:None, 4:None, 5:None, 6:"Salsa night"}},
     {"name": "Cafe de Paris", "parties": {0:None, 1:None, 2:None, 3:"Boston tea party", 4:None, 5:None, 6:None}},
     {"name": "Circe le Soir", "parties": {0:None, 1:None, 2:None, 3:None, 4:"Baszataska", 5:None, 6:None}}
 ]
